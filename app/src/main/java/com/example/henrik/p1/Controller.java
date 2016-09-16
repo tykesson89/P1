6298fe28e8b02d65c2c94e707243f770111997c6
Henrik <tykesson89@gmail.com>
2016-09-16 02:11:24 +0200

    första

--- app/src/main/java/com/example/henrik/p1/Controller.java
+++ app/src/main/java/com/example/henrik/p1/Controller.java
@@ -11,18 +11,14 @@ import android.widget.SpinnerAdapter;
  * Created by Henrik on 2016-09-09.
  */
 public class Controller {
-    private Activity activity;
-    private MainFragment mainFragment;
-    private FragmentIncome fragmentIncome;
+
 
 
 
     public Controller(Activity activity, MainFragment mainFragment, FragmentIncome fragmentIncome ){
         mainFragment.setController(this);
         fragmentIncome.setController(this);
-        this.activity = activity;
-        this.mainFragment = mainFragment;
-        this.fragmentIncome = fragmentIncome;
+
         initResources(activity);
 
     }
